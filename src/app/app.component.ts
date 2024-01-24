55535ae8bab2e03181ad06df30b2c1aad5bde1bf
Ravi Gangawane <developer.ravi08@gmail.com>
2024-01-24 13:38:28 +0530

    update index and docs for public deployment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { RouterModule, RouterOutlet } from '@angular/router';
 import { IPostCardComponent } from './components/advance/i-post-card/i-post-card.component';
 import { CreatePostCardComponent } from './components/advance/create-post-card/create-post-card.component';
 import { BasicComponent } from './components/basic/basic.component';
+import { PostType } from './models/post';
 
 @Component({
   selector: 'app-root',
@@ -12,6 +13,33 @@ import { BasicComponent } from './components/basic/basic.component';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'angular-17-crud-operations-with-ngrx';
+  title = 'Angular 17 CRUD Operations with ngRx';
+  postList: PostType[] = [
+    {
+      postId: 1,
+      userProfileUrl: "../../../assets/logo/user.png",
+      userName: "Ravi G",
+      userDesc: "Testing tmp Off",
+      postUrl: "../../../assets/images/LOGO.png",
+      postDesc: "testing",
+      postDate: "",
+      isLike: true,
+      isCommnet: false,
+      isShare: false,
+      isSave: true,
+      likeCount: 10,
+      commentCount: 10,
+      commentList: [],
+    }
+  ]
+
+  constructor() {
+    if (localStorage.getItem("postList") === null) {
+      console.log('No List',)
+      localStorage.setItem('postList', JSON.stringify(this.postList))
+    } else {
+      console.log('Is List',)
+    }
+  }
 
 }
--- src/app/components/basic/basic.component.ts
+++ src/app/components/basic/basic.component.ts
@@ -52,7 +52,6 @@ export class BasicComponent {
 
   constructor(private service: DataService) {
 
-
   }
 
   ngOnInit() {
